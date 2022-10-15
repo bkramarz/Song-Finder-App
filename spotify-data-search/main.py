b827ea5b827ae314de3ac788f3b8a94c0c283d07
bkramarz <105524805+bkramarz@users.noreply.github.com>
2022-10-14 17:50:16 -0700

    Update main.py

--- spotify-data-search/main.py
+++ spotify-data-search/main.py
@@ -5,12 +5,12 @@ spotify_search = SpotifySearch()
 
 query = input("Enter an artist name:\n")
 
-# spotify_similar_artist_list = spotify_search.get_related_artists(query)
-# last_fm_similar_artist_list = last_fm_search.get_similar_artists(query)
-#
-# final_list = [artist for artist in last_fm_similar_artist_list if artist in spotify_similar_artist_list]
-#
-# print(final_list)
+spotify_similar_artist_list = spotify_search.get_related_artists(query)
+last_fm_similar_artist_list = last_fm_search.get_similar_artists(query)
+
+final_list = [artist for artist in last_fm_similar_artist_list if artist in spotify_similar_artist_list]
+
+print(final_list)
 
 last_fm_top_tracks = LastFmSearch.get_top_tracks(query)
 spotify_top_tracks = spotify_search.get_top_tracks(query)
